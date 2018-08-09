c4a69bc2c5ba796b940b1a0a6f5a39bc9f92d3be
bwangel23 <bwangel.me@gmail.com>
2018-08-09 23:36:51 +0800

    add mqtt client test

--- client.go
+++ client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"sync"
 
@@ -12,7 +11,7 @@ type MQTTClient struct {
 	addr      string
 	username  string
 	password  string
-	wg        sync.WaitGroup
+	wg        *sync.WaitGroup
 	rawClient mqtt.Client
 }
 
@@ -33,7 +32,7 @@ func NewMQTTClient(addr, username, password string) *MQTTClient {
 		addr:      addr,
 		username:  username,
 		password:  password,
-		wg:        wg,
+		wg:        &wg,
 		rawClient: client,
 	}
 }
@@ -55,17 +54,6 @@ func (self *MQTTClient) Pub(topic string, message string) {
 
 }
 
-func main() {
-	const TOPIC = "mytopic/test"
-
-	client := NewMQTTClient("tcp://localhost:1883", "admin", "public")
-
-	client.Sub(TOPIC, func(client mqtt.Client, msg mqtt.Message) {
-		fmt.Printf("Get the message `%s`\n", msg.Payload())
-	})
-
-	client.Pub(TOPIC, "hello, xff1")
-	client.Pub(TOPIC, "hello, xff2")
-
-	client.wg.Wait()
+func (self *MQTTClient) Wait() {
+	self.wg.Wait()
 }
--- client_test.go
+++ client_test.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"testing"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func TestMqttPubSub(t *testing.T) {
+	const TOPIC = "mytopic/test"
+	post_message := "hello, xff1"
 
+	client := NewMQTTClient("tcp://localhost:1883", "admin", "public")
+
+	client.Sub(TOPIC, func(client mqtt.Client, msg mqtt.Message) {
+		message := string(msg.Payload())
+		if post_message != message {
+			t.Fatalf("%s != %s\n", post_message, message)
+		}
+	})
+
+	client.Pub(TOPIC, post_message)
+	client.Wait()
 }

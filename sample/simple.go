eda063c7fc25ffb27c703bc59879160bd6956e64
bwangel23 <bwangel.me@gmail.com>
2018-08-10 22:42:50 +0800

    update on

---
+++ sample/simple.go
@@ -0,0 +1,16 @@
+package sample
+
+import (
+	"fmt"
+
+	"github.com/eclipse/paho.mqtt.golang"
+)
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	p
+}
